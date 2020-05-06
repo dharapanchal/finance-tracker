b0d15787a3534bf0411ed9b797fa427610894718
Dhara Panchal <dhara.panchal@einfochips.com>
2020-05-06 16:03:48 +0530

    Add search method which search based on fname,lname and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,22 +8,49 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   def stock_already_tracked?(ticker_symbol)
-     stock = Stock.check_db(ticker_symbol)
-     return false unless stock
-     stocks.where(id: stock.id).exists?
-   end
+  def stock_already_tracked?(ticker_symbol)
+   stock = Stock.check_db(ticker_symbol)
+   return false unless stock
+   stocks.where(id: stock.id).exists?
+  end
 
-   def under_stock_limit?
-     stocks.count < 5
-   end
+  def under_stock_limit?
+    stocks.count < 5
+  end
 
-   def can_track_stock?(ticker_symbol)
-     under_stock_limit? && !stock_already_tracked?(ticker_symbol)
-   end
+  def can_track_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_tracked?(ticker_symbol)
+  end
 
-   def full_name
+  def full_name
     return "#{first_name} #{last_name}" if first_name || last_name
-   end
+  end
+
+  def self.search(param)
+    param.strip!
+    to_send_back = (first_name(param) + last_name(param) + email(param)).uniq
+    retrun nil unless to_send_back
+    to_send_back
+  end
+
+  def self.first_name(param)
+    matches('first_name',param)
+  end
+
+  def self.last_name(param)
+    matches('last_name',param)
+  end
+
+  def self.email(param)
+    matches('email',param)
+  end
+
+  def self.matches(field_name, param)
+    User.where("#{field_name} like ?","%#{param}%")
+  end
+
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id }
+  end
 
 end
